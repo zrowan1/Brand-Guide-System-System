b733f263529de58a3f24355b13f23efd353a3e67
rowandeschaux <483907@student.fontys.nl>
2022-03-24 12:20:14 +0100

    App.js aangepast om data overdracht van node back-end naar react frond-end mogelijk te maken

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,19 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+    .then((res) => res.json())
+    .then((data) => setData(data.message));
+  })
+
+
+
   return (
     <div className="App">
       <header className="App-header">
